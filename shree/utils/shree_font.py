014b1db5ebfa516f246695b84054cc851e505f82
Suraj08832 <rs3268840@gmail.com>
2025-06-05 22:02:15 +0530

    Add: Created shree_font.py with font styles

---
+++ shree/utils/shree_font.py
@@ -0,0 +1,120 @@
+class Fonts:
+    def typewriter(text):
+        return "𝚝𝚢𝚙𝚎𝚠𝚛𝚒𝚝𝚎𝚛"
+
+    def outline(text):
+        return "𝕠𝕦𝕥𝕝𝕚𝕟𝕖"
+
+    def serief(text):
+        return "𝐬𝐞𝐫𝐢𝐟"
+
+    def bold_cool(text):
+        return "𝒃𝒐𝒍𝒅 𝒄𝒐𝒐𝒍"
+
+    def cool(text):
+        return "𝑐𝑜𝑜𝑙"
+
+    def smallcap(text):
+        return "sᴍᴀʟʟ ᴄᴀᴘs"
+
+    def script(text):
+        return "𝓈𝒸𝓇𝒾𝓅𝓉"
+
+    def bold_script(text):
+        return "𝓫𝓸𝓵𝓭 𝓼𝓬𝓻𝓲𝓹𝓽"
+
+    def tiny(text):
+        return "ᵗⁱⁿʸ"
+
+    def comic(text):
+        return "ᑕOᗰIᑕ"
+
+    def san(text):
+        return "𝗦𝗮𝗻𝘀"
+
+    def slant_san(text):
+        return "𝙎𝙖𝙣𝙨"
+
+    def slant(text):
+        return "𝘚𝘢𝘯𝘴"
+
+    def sim(text):
+        return "𝖲𝖺𝗇𝗌"
+
+    def circles(text):
+        return "Ⓒ︎Ⓘ︎Ⓡ︎Ⓒ︎Ⓛ︎Ⓔ︎Ⓢ︎"
+
+    def dark_circle(text):
+        return "🅒︎🅘︎🅡︎🅒︎🅛︎🅔︎🅢︎"
+
+    def gothic(text):
+        return "𝔊𝔬𝔱𝔥𝔦𝔠"
+
+    def bold_gothic(text):
+        return "𝕲𝖔𝖙𝖍𝖎𝖈"
+
+    def cloud(text):
+        return "C͜͡l͜͡o͜͡u͜͡d͜͡s͜͡"
+
+    def happy(text):
+        return "H̆̈ă̈p̆̈p̆̈y̆̈"
+
+    def sad(text):
+        return "S̑̈ȃ̈d̑̈"
+
+    def magical(text):
+        return "ɱαɠι¢αℓ"
+
+    def special(text):
+        return "🇸 🇵 🇪 🇨 🇮 🇦 🇱"
+
+    def square(text):
+        return "🅂🅀🅄🄰🅁🄴🅂"
+
+    def dark_square(text):
+        return "🆂︎🆀︎🆄︎🅰︎🆁︎🅴︎🆂︎"
+
+    def andalucia(text):
+        return "ꪖꪀᦔꪖꪶꪊᥴ𝓲ꪖ"
+
+    def manga(text):
+        return "爪卂几ᘜ卂"
+
+    def stinky(text):
+        return "S̾t̾i̾n̾k̾y̾"
+
+    def bubbles(text):
+        return "B̥ͦu̥ͦb̥ͦb̥ͦl̥ͦe̥ͦs̥ͦ"
+
+    def underline(text):
+        return "U͟n͟d͟e͟r͟l͟i͟n͟e͟"
+
+    def ladybug(text):
+        return "꒒ꍏꀷꌩꌃꀎꁅ"
+
+    def rays(text):
+        return "R҉a҉y҉s҉"
+
+    def birds(text):
+        return "B҈i҈r҈d҈s҈"
+
+    def slash(text):
+        return "S̸l̸a̸s̸h̸"
+
+    def stop(text):
+        return "s⃠t⃠o⃠p⃠"
+
+    def skyline(text):
+        return "S̺͆k̺͆y̺͆l̺͆i̺͆n̺͆e̺͆"
+
+    def arrows(text):
+        return "A͎r͎r͎o͎w͎s͎"
+
+    def rvnes(text):
+        return "ዪሀክቿነ"
+
+    def strike(text):
+        return "S̶t̶r̶i̶k̶e̶"
+
+    def frozen(text):
+        return "F༙r༙o༙z༙e༙n༙" 
